cf7a8facc4c15bcacf109f2cc2e4b8a9d60f9e8a
Максим Сын Отца Своего <noimxl1@gmail.com>
2025-02-04 11:03:34 +0300

    04.02

--- assets/js/header-scroll-script.js
+++ assets/js/header-scroll-script.js
@@ -6,3 +6,15 @@ window.addEventListener('scroll', () => {
         header.style.backgroundColor = '';
     }
 });
+if(document.getElementById('burger-menu')){
+    const burgerMenu = document.getElementById('burger-menu');
+    const menuContent = document.querySelector('header nav');
+    const header = document.querySelector('header');
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('open');
+        menuContent.classList.toggle('open');
+        header.classList.toggle('open');
+
+    });
+
+}
--- assets/js/mobile-menu-script.js
+++
@@ -1,5 +0,0 @@
-const burgerMenu = document.getElementById('burger-menu');
-
-burgerMenu.addEventListener('click', () => {
-    burgerMenu.classList.toggle('open');
-});
